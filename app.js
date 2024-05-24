66a641031621a75571d1b647ad1d4bb83f345d39
vanderson22 <vanderson.01@hotmail.com>
2024-05-24 18:18:57 +0000

    including basic routes

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-//imports
-const http = require('http');
-
 
 //imports 3rd party
 const express = require('express');
@@ -8,7 +5,25 @@ const express = require('express');
 const app = express();
 
 console.log('Starting server...');
-const server = http.createServer(app);
+// const server = http.createServer(app);
+// server.listen(3000);
 
-server.listen(3000);
+// app.use((request, response, next)=>{
+
+//     next();
+// });
+
+app.use('/add-product', (request, response, next)=>{
+
+    response.send('<h1> Welcome to product page.</h1>');
+});
+
+
+app.use('/',(request, response, next)=>{
+
+    response.send('<h1> Welcome home page.</h1>');
+});
+
+
+app.listen(3000)
 console.log('... server started.');
