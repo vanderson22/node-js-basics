dd036bad018877f5551377eea84cbe48feeb8db7
vanderson22 <vanderson.01@hotmail.com>
2024-05-24 11:49:09 +0000

    express js installed

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 //imports
 const http = require('http');
-const routes = require('./routes');
+
+
+//imports 3rd party
+const express = require('express');
+
+const app = express();
 
 console.log('Starting server...');
-const server = http.createServer(routes.handle);
+const server = http.createServer(app);
 
 server.listen(3000);
 console.log('... server started.');
