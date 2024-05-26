fb3f2bece84629a5e87bee3a9ca2dccd0be519a0
vanderson22 <vanderson.01@hotmail.com>
2024-05-26 13:50:50 +0000

    limiting middleware to post request

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
+const users = require('./users');
+
 
 //imports 3rd party
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+//to parse incoming request.
+//app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}));
 
 console.log('Starting server...');
 // const server = http.createServer(app);
@@ -13,15 +19,33 @@ console.log('Starting server...');
 //     next();
 // });
 
-app.use('/add-product', (request, response, next)=>{
+app.use('/add-products', (request, response, next)=>{
 
-    response.send('<h1> Welcome to product page.</h1>');
+    response.send(`<html> 
+    <h1> Welcome to assignment page! </h1>
+     <br /> 
+     <label>insert your product: </label> 
+     <form action = "/product" method="POST" > 
+        <input type="text" name='product' placeholder="product"> 
+        <button type="submit">Send</button>
+     </form>
+
+  <html>`);
+});
+
+app.post('/product',(request, response, next)=>{
+    //nesse ponto body é um objeto, não uma string ( tente colocar em uma string para ver o erro.)
+    console.log(request.body);
+    
+    //redirecionamento.
+    response.redirect('/');
 });
 
 app.use('/',(request, response, next)=>{
-    console.log('running middleware...');
+    console.log('running middleware first...');
      next();
 });
+
 
 app.use('/',(request, response, next)=>{
 
@@ -29,5 +53,5 @@ app.use('/',(request, response, next)=>{
 });
 
 
-app.listen(3000)
+app.listen(3000);
 console.log('... server started.');
