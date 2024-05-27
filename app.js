35925196accba36abeda7ac58db210a3bdeafea0
vanderson22 <vanderson.01@hotmail.com>
2024-05-27 14:31:27 +0000

    71. returning page 404

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const path = require('path');
+
 const app = express();
 
 const adminRoutes = require('./routes/admin');
@@ -12,10 +14,10 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use("/" , (req, resp) =>{
+app.use("/" , (req, resp, next) =>{
 
 
-    resp.send('<h1> Page not found! </h1>')
+    resp.status(404).sendFile( path.join( __dirname, 'views', 'page-not-found.html'));
 } );
 
 app.listen(3000);
